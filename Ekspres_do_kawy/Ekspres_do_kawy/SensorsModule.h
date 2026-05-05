2d8cf7bdc837e64ce2b9e8654e2396a3eb2471aa
hanslik23 <k.hanslik12@gmail.com>
2026-05-05 21:02:53 +0200

    Stworzenie projektu

---
+++ Ekspres_do_kawy/Ekspres_do_kawy/SensorsModule.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+class SensorsModule
+{
+public:
+	void checksensors();
+	bool isReadytobrew();		
+
+private:
+
+	int checkwater();			// logika odczytu poziomu wody
+	int checkbeans();			// logika odczytu poziomu ziaren 
+	int checkgrounds();		// logika odczytu poziomu fusów
+};
+
